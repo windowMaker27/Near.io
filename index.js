12bbfb52085bb8c424bda620972a278b07c703ac
windowMaker27 <t.virassamy270@gmail.com>
2026-06-17 22:40:30 +0200

    feat : index

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 // Guard: empeche les polyfills de redefinir les props read-only de Event
 // (NONE, CAPTURING_PHASE, AT_TARGET, BUBBLING_PHASE) sur Hermes + New Arch.
 // Doit etre execute AVANT tout import, notamment avant expo/winter/runtime.
-//  if (typeof global.Event !== 'undefined') {
-//    const _defineProperty = Object.defineProperty;
-//    Object.defineProperty = function(obj, prop, descriptor) {
-//      if (obj === global.Event && ['NONE','CAPTURING_PHASE','AT_TARGET','BUBBLING_PHASE'].includes(prop)) {
-//        return obj; // ignore silencieusement
-//      }
-//      return _defineProperty(obj, prop, descriptor);
-//    };
-//  }
-//  
-//  // DOMException shim - Hermes ne l'expose pas globalement
-//  if (typeof global.DOMException === 'undefined') {
-//    global.DOMException = class DOMException extends Error {
-//      constructor(message, name) {
-//        super(message);
-//        this.name = name || 'Error';
-//      }
-//    };
-//  }
-//  
+if (typeof global.Event !== 'undefined') {
+  const _defineProperty = Object.defineProperty;
+  Object.defineProperty = function(obj, prop, descriptor) {
+    if (obj === global.Event && ['NONE','CAPTURING_PHASE','AT_TARGET','BUBBLING_PHASE'].includes(prop)) {
+      return obj; // ignore silencieusement
+    }
+    return _defineProperty(obj, prop, descriptor);
+  };
+}
+
+// DOMException shim - Hermes ne l'expose pas globalement
+if (typeof global.DOMException === 'undefined') {
+  global.DOMException = class DOMException extends Error {
+    constructor(message, name) {
+      super(message);
+      this.name = name || 'Error';
+    }
+  };
+}
+
 console.log("AbortSignal =", global.AbortSignal);
 console.log("AbortController =", global.AbortController);
 
