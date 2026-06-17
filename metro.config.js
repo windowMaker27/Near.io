d2040069e65e9c35baa3484e86ad91888efc5c20
windowMaker27 <t.virassamy270@gmail.com>
2026-06-17 18:51:40 +0200

    fix: supprimer unstable_enablePackageExports qui casse le transformer Metro

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,3 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
-
-config.resolver = {
-  ...config.resolver,
-  unstable_enablePackageExports: true,
-};
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
